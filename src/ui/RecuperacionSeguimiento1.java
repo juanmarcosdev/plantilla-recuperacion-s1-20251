930c0ad8b955e4a37672898bdab8eddc08f981b9
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2025-02-26 10:40:49 -0500

    Agregando archivo base

---
+++ src/ui/RecuperacionSeguimiento1.java
@@ -0,0 +1,51 @@
+package ui;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class RecuperacionSeguimiento1 {
+
+	private Scanner escaner;
+
+    // Declaracion de constantes 
+
+	private RecuperacionSeguimiento1() {
+		escaner = new Scanner(System.in);
+	}
+	
+	
+	public void run() // Aqui en el metodo run hacemos lo de nuestro metodo main. Solo entrada de datos, mensajes al usuario y mostrar salidas
+	{   
+        // Metodo pow de Math
+        double a = 2;
+        double b = 3;
+        System.out.println(Math.pow(a, b));
+        
+
+        // Declaracion de info a pedir del usuario 
+        
+        // Pedir info al usuario 
+
+        // Declaracion de salidas calculadas 
+       
+        // Calculo de salidas mediante metodos 
+        
+
+        // Mostramos las salidas al usuario
+        
+	}
+
+	public static void main(String[] args) {
+		RecuperacionSeguimiento1 mainApp = new RecuperacionSeguimiento1();
+		mainApp.run();
+	}
+
+    /**
+	 * Descripción: El método ... permite ...
+	 * @param 
+	 * @return 
+	 */
+	
+
+	
+}
